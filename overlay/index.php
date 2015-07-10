402807ee9c1eaea08637d496d8bb04b1df42844e
DocCyblade <ken@robinsonshome.net>
2015-07-10 07:33:42 +0000

    Add base web control panel and make file updates for adminer
    Updates make file to add common adminer changes for postgres
    
    Updates make file to add common overlay and conf scripts for the
    web control panel (tkl-webcp)
    
    Adds a base index.php control panel file to be edited with a
    conf script

---
+++ overlay/index.php
@@ -0,0 +1,63 @@
+<!-- TKL Control Panel Template - Use sed to edit. Use TKLCPL comment to help insert lines -->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="Content-Style-Type" content="text/css">
+        <meta http-equiv="Content-Script-Type" content="text/javascript">
+
+        <title>TurnKey APP_NAME_HERE</title>
+        
+        <link rel="stylesheet" href="css/ui.tabs.css" type="text/css" media="print, projection, screen"/>
+        <link rel="stylesheet" href="css/base.css" type="text/css"/>
+
+        <script src="js/jquery-1.2.6.js" type="text/javascript"></script>
+        <script src="js/ui.core.js" type="text/javascript"></script>
+        <script src="js/ui.tabs.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            $(function() {
+                $('#container-1 > ul').tabs({ fx: { opacity: 'toggle'} });
+            });
+        </script>
+    </head>
+
+    <body>
+        <h1>TurnKey APP_NAME_HERE</h1>
+        
+        <div id="container-1">
+            <ul>
+                <li><a href="#cp"><span>Control Panel</span></a></li>
+            </ul>
+
+            <div id="cp">
+                <div class="fragment-content">
+                    <div>
+                        <a href="https://<?php print
+                        $_SERVER{'HTTP_HOST'}; ?>:12320"><img
+                        src="images/shell.png"/>Web Shell</a>
+                    </div>
+                    <div>
+                        <a href="https://<?php print
+                        $_SERVER{'HTTP_HOST'}; ?>:12321"><img
+                        src="images/webmin.png"/>Webmin</a>
+                    </div>
+                    
+                    <!-- Use sed to add to area use div tags-->
+                    <!-- TKLCPL_CP_AREA -->
+                    
+                    
+                    
+                    <h2>Resources and references</h2>
+                    <ul>
+                        <!-- Use sed to add to area. Use ul tags-->
+                        <!-- TKLCPL_REF_AREA -->
+                        
+                    </ul>
+
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
