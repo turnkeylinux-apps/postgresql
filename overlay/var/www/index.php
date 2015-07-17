b90610ee43ff0d0dde729dfc47ed9478760f360f
DocCyblade <ken@robinsonshome.net>
2015-07-16 22:48:55 -0400

    Update index.php to be static
    Instead of using a base index file, just going to have one for via
    the overlay for now

--- overlay/var/www/index.php
+++ overlay/var/www/index.php
@@ -1,4 +1,3 @@
-<!-- TKL Control Panel Template - Use sed to edit. Use TKLCPL comment to help insert lines -->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
 <html lang="en">
@@ -7,10 +6,10 @@
         <meta http-equiv="Content-Style-Type" content="text/css">
         <meta http-equiv="Content-Script-Type" content="text/javascript">
 
-        <title>TurnKey APP_NAME_HERE</title>
+        <title>TurnKey PostgreSQL</title>
         
-        <link rel="stylesheet" href="css/ui.tabs.css" type="text/css" media="print, projection, screen"/>
-        <link rel="stylesheet" href="css/base.css" type="text/css"/>
+        <link rel="stylesheet" href="css/ui.tabs.css" type="text/css" media="print, projection, screen">
+        <link rel="stylesheet" href="css/base.css" type="text/css">
 
         <script src="js/jquery-1.2.6.js" type="text/javascript"></script>
         <script src="js/ui.core.js" type="text/javascript"></script>
@@ -23,7 +22,7 @@
     </head>
 
     <body>
-        <h1>TurnKey APP_NAME_HERE</h1>
+        <h1>TurnKey PostgreSQL</h1>
         
         <div id="container-1">
             <ul>
@@ -42,17 +41,19 @@
                         $_SERVER{'HTTP_HOST'}; ?>:12321"><img
                         src="images/webmin.png"/>Webmin</a>
                     </div>
-                    
-                    <!-- Use sed to add to area use div tags-->
-                    <!-- TKLCPL_CP_AREA -->
-                    
-                    
-                    
+                    <div>
+                        <a href="https://<?php print
+                        $_SERVER{'HTTP_HOST'}; ?>:12322"><img
+                        src="images/adminer.png"/>Adminer</a>
+                    </div>
+                    <div></div>
+                    <div></div>
+
                     <h2>Resources and references</h2>
                     <ul>
-                        <!-- Use sed to add to area. Use ul tags-->
-                        <!-- TKLCPL_REF_AREA -->
-                        
+                        <li><a
+                        href="http://www.turnkeylinux.org/postgresql">
+                        TurnKey PostgreSQL release notes</a></li>
                     </ul>
 
                 </div>
